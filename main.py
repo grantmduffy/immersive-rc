9107e13dce2744b1124d5985e7a88103d1d1b377
grant <grantmduffy@gmail.com>
2023-05-27 10:49:36 -0700

    added lap recording

--- main.py
+++ main.py
@@ -7,18 +7,26 @@ import numpy as np
 import cv2
 from threading import Thread
 from pywinusb import hid
-from matplotlib import pyplot as plt
+from datetime import datetime
+from pathlib import Path
+import keyboard
+
+import yaml
+
+with open('config.yaml', 'r') as f:
+    config = yaml.safe_load(f)
 
 display_res = (1280, 960)
 
 inputs = {'steering': 0.0, 'throttle': 0.0, 'clutch': 0.0, 'brake': 0.0}
 steering_pos = 0
 packet_rate = 500
-frame_rate = 60
+frame_rate = 24
 battery_level = 0
 
 run = False
-camera = 0
+lap = False
+camera = config['camera']
 n_trace = 400
 trace_scale = 2
 trace_height = 100
@@ -27,16 +35,38 @@ lines = np.zeros((3, n_trace, 2), int)
 lines[..., 0] = np.arange(n_trace)[None, None, :] * trace_scale
 
 
+def write_frame(out, frame: np.ndarray, csv):
+    global inputs, frame_rate
+    t = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    t_micros = datetime.now().strftime('%H-%M-%S.%f')
+    if out is None:
+        out = cv2.VideoWriter(
+            str(Path(config['lap_video_path']) / f'{t}.avi'), cv2.VideoWriter_fourcc(*'MJPG'), 
+            frame_rate, (frame.shape[1], frame.shape[0])
+        )
+        csv = Path(config['lap_video_path']) / f'{t}.csv'
+        with open(csv, 'w') as f:
+            f.write('time,steering,throttle,battery\n')
+    out.write(frame)
+    with open(csv, 'a') as f:
+        f.write(','.join(str(x) for x in (t_micros, inputs['steering'], inputs['throttle'], battery_level)) + '\n')
+    return out, csv
+
+
+
 def camera_thread():
-    global run, frame_rate, packet_rate, battery_level, trace_buffer
-    # cap = cv2.VideoCapture(camera)
+    global run, lap, frame_rate, packet_rate, battery_level, trace_buffer
+    cap = cv2.VideoCapture(camera)
+    vid_out = None
+    csv_out = None
     run = True
     while run:
-        # ret, frame = cap.read()
         t0 = perf_counter()
-        ret, frame = True, np.random.randint(0, 255, (480, 640, 3), np.uint8)
+        ret, frame = cap.read()
+        # ret, frame = True, np.random.randint(0, 255, (480, 640, 3), np.uint8)
         if not ret:
             continue
+        vid_out, csv_out = write_frame(vid_out, frame, csv_out)
         trace_buffer = np.roll(trace_buffer, 1, 1)
         trace_buffer[:, 0] = battery_level, frame_rate, packet_rate
         display_frame = cv2.resize(frame, display_res)
@@ -53,6 +83,10 @@ def camera_thread():
             )):
             display_frame = cv2.putText(display_frame, t, (10, trace_height + 30 + i * 30), 
                                         cv2.FONT_HERSHEY_COMPLEX, 1, c, 2)
+        
+        display_frame = cv2.putText(display_frame, csv_out.name.replace('.csv', ''),
+                                    (10, trace_height + 120), cv2.FONT_HERSHEY_COMPLEX, 
+                                    1, (0, 0, 0), 2)
 
         cv2.imshow('FPV Camera', display_frame)
         cv2.waitKey(1)
@@ -60,6 +94,15 @@ def camera_thread():
             run = False
         t1 = perf_counter()
         frame_rate = 1 / (t1 - t0)
+
+        if lap:
+            vid_out.release()
+            vid_out = None
+            csv_out = None
+            lap = False
+    
+    if vid_out is not None:
+        vid_out.release()
 
 
 def input_callback(x):
@@ -74,7 +117,7 @@ def controller_callback(vals):
     inputs['throttle'] = map_value(throttle, (0, 65534), (0, 1)) if throttle < 65534 else 0
 
 
-def get_port(baudrate=2000000):
+def get_port():
     available_ports = comports()
     if len(available_ports) == 0:
         print('No COM ports available')
@@ -85,12 +128,26 @@ def get_port(baudrate=2000000):
         print('Available COM ports:')
         print('\n'.join([x.name for x in available_ports]))
         port = input('Which port would you like to use?')
-    return baudrate, port
+    return port
+
+
+def stop(_):
+    global run
+    run = False
+
+
+def finish_lap(_):
+    global lap
+    lap = True
 
 
 if __name__ == '__main__':
 
-    Thread(target=camera_thread).start()
+    cam_thread = Thread(target=camera_thread)
+    cam_thread.start()
+    keyboard.on_press_key('esc', stop)
+    keyboard.on_press_key('space', finish_lap)
+    # camera_thread()
 
     # device = hid.HidDeviceFilter(vendor_id=0x045e, product_id=0x0000).get_devices()[0]
     device = hid.HidDeviceFilter(vendor_id=0x045e, product_id=0x028e).get_devices()[0]  # XBox 360 controller
@@ -98,7 +155,8 @@ if __name__ == '__main__':
     device.set_raw_data_handler(controller_callback)
 
     # connect serial
-    baud, port = get_port()
+    port = get_port()
+    baud = config['baudrate']
     print(f'Connecting to {port}')
 
     # with LogiWheel() as wheel, serial.Serial(port, timeout=0.1, baudrate=baudrate) as ser:
@@ -158,8 +216,11 @@ if __name__ == '__main__':
             packet_rate = 1 / (t1 - t0)
 
             # stop if battery gets too low
-            if battery_level < 7.5:
-                run = False
+            # if battery_level < 7.5:
+            #     run = False
 
             # print(f'\r{1 / (t1 - t0):7.2f}Hz', f'{steering_pos:5.3f}, steering={steering_pos:6.4f}, throttle={throttle_force:5.3}, bat={bat_val:.2f}V', end='    ')
             # sleep(1 / 60)
+    
+    cam_thread.join()
+
